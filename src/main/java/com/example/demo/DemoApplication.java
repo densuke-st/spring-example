869c473dd5e7e9862f2ad4fe7a4be87738bb635f
hisamatunao <kd1329331@st.kobedenshi.ac.jp>
2023-12-25 21:18:22 +0900

    [add] 日本語化（もどき）

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
     public static void main(String[] args) {
-         SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(DemoApplication.class, args);
     }
     @GetMapping("/hello")
     public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
